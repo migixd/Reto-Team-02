16e36cc1188d08db82dd7e44357ebcc435cfc66a
Andres Alcocoer Urueta <a01567307@tec.mx>
2026-03-19 14:59:06 -0600

    movimeinto

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -20,6 +20,40 @@ public class Move : MonoBehaviour
             Vector3 direction = new Vector3(0, 0, 1);
             transform.Translate(direction * speed * Time.deltaTime);
         }
-      
+        else if (Keyboard.current.sKey.isPressed)
+        {
+            Vector3 direction = new Vector3(0, 0, -1);
+            transform.Translate(direction * speed * Time.deltaTime);
+        }
+        else if (Keyboard.current.aKey.isPressed)
+        {
+            Vector3 direction = new Vector3(-1, 0, 0);
+            transform.Translate(direction * speed * Time.deltaTime);
+        }
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            Vector3 direction = new Vector3(1, 0, 0);
+            transform.Translate(direction * speed * Time.deltaTime);
+        } 
+        else if (Keyboard.current.wKey.isPressed && Keyboard.current.aKey.isPressed)
+        {
+            Vector3 direction = new Vector3(-1, 0, 1).normalized;
+            transform.Translate(direction * speed * Time.deltaTime);
+        }
+        else if (Keyboard.current.wKey.isPressed && Keyboard.current.dKey.isPressed)
+        {
+            Vector3 direction = new Vector3(1, 0, 1).normalized;
+            transform.Translate(direction * speed * Time.deltaTime);
+        }
+        else if (Keyboard.current.sKey.isPressed && Keyboard.current.aKey.isPressed)
+        {
+            Vector3 direction = new Vector3(-1, 0, -1).normalized;
+            transform.Translate(direction * speed * Time.deltaTime);
+        }
+        else if (Keyboard.current.sKey.isPressed && Keyboard.current.dKey.isPressed)
+        {
+            Vector3 direction = new Vector3(1, 0, -1).normalized;
+            transform.Translate(direction * speed * Time.deltaTime);
+        }
     }
 }
